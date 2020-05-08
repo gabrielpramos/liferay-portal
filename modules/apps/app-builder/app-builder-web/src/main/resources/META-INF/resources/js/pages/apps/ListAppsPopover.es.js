eda95ad5d24d75ad8a67dc19f0eb883eeba2050a
Nara Andrade <nara.andrade@liferay.com>
2020-05-08 15:17:33 -0300

    WIP

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListAppsPopover.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListAppsPopover.es.js
@@ -13,10 +13,13 @@
  */
 
 import Button from '@clayui/button';
-import React, {useRef} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 
+import {AppContext} from '../../AppContext.es';
 import DropDown from '../../components/drop-down/DropDown.es';
 import Popover from '../../components/popover/Popover.es';
+import {addItem} from '../../utils/client.es';
+import CustomObjectPopover from '../custom-object/CustomObjectPopover.es';
 
 const ListAppsPopover = ({
 	alignElement,
@@ -25,15 +28,30 @@ const ListAppsPopover = ({
 	onSubmit,
 	visible,
 }) => {
+	const {basePortletURL} = useContext(AppContext);
 	const objectInputRef = useRef();
+	const customObjectButtonRef = useRef();
+	const popoverRef = useRef();
+
+	const [emptyAlignElement, setEmptyAlignElement] = useState(
+		customObjectButtonRef.current
+	);
+	const [isPopoverVisible, setPopoverVisible] = useState(false);
 
 	const resetForm = () => {
 		objectInputRef.current.value = '';
 	};
 
-	const emptyProps = {
-		emptyButtonOnClick: {
-			//manage customObjectPopover visibility
+	const EMPTY_PROPS = {
+		customObjectButtonRef,
+		emptyButtonOnClick: ({currentTarget}) => {
+			setEmptyAlignElement(currentTarget);
+
+			if (isPopoverVisible && emptyAlignElement !== currentTarget) {
+				return;
+			}
+
+			setPopoverVisible(!isPopoverVisible);
 		},
 		emptyStateButton: Liferay.Language.get('new-custom-object'),
 		emptyStateLabel: Liferay.Language.get(
@@ -41,7 +59,7 @@ const ListAppsPopover = ({
 		),
 	};
 
-	const errorProps = {
+	const ERROR_PROPS = {
 		errorButtonOnClick: {
 			//retry
 		},
@@ -49,72 +67,109 @@ const ListAppsPopover = ({
 		errorStateLabel: Liferay.Language.get('failed-to-retrieve-objects'),
 	};
 
-	const loadingProps = {
-		loadingStateIcon: '',
+	const LOADING_PROPS = {
 		loadingStateLabel: Liferay.Language.get('retrieving-all-objects'),
 	};
 
+	const handleOnCancel = () => setPopoverVisible(false);
+
+	const handleOnSubmit = ({isAddFormView, name}) => {
+		const addURL = `/o/data-engine/v2.0/data-definitions/by-content-type/app-builder`;
+
+		addItem(addURL, {
+			availableLanguageIds: ['en_US'],
+			dataDefinitionFields: [],
+			name: {
+				value: name,
+			},
+		}).then(({id}) => {
+			if (isAddFormView) {
+				Liferay.Util.navigate(
+					Liferay.Util.PortletURL.createRenderURL(basePortletURL, {
+						dataDefinitionId: id,
+						mvcRenderCommandName: '/edit_form_view',
+						newCustomObject: true,
+					})
+				);
+			} else {
+				history.push(`/custom-object/${id}/form-views/`);
+			}
+		});
+	};
+
 	return (
-		<Popover
-			alignElement={alignElement}
-			className="apps-popover mw-100"
-			content={() => (
-				<>
-					<label>{Liferay.Language.get('object')}</label>
-					<DropDown
-						displayType="secondary"
-						emptyProps={emptyProps}
-						errorProps={errorProps}
-						items={[]}
-						label={Liferay.Language.get('select-object')}
-						loadingProps={loadingProps}
-					>
-						<DropDown.Search />
-					</DropDown>
-				</>
-			)}
-			footer={() => (
-				<div className="border-top mt-3 p-3" style={{width: 450}}>
-					<div className="d-flex justify-content-end">
-						<Button
-							className="mr-3"
+		<>
+			<Popover
+				alignElement={alignElement}
+				className="apps-popover mw-100"
+				content={() => (
+					<>
+						<label>{Liferay.Language.get('object')}</label>
+						<DropDown
 							displayType="secondary"
-							onClick={() => {
-								resetForm();
-
-								onCancel();
-							}}
-							small
+							emptyProps={EMPTY_PROPS}
+							errorProps={ERROR_PROPS}
+							items={[]}
+							label={Liferay.Language.get('select-object')}
+							loadingProps={LOADING_PROPS}
 						>
-							{Liferay.Language.get('cancel')}
-						</Button>
+							<DropDown.Search />
+						</DropDown>
+					</>
+				)}
+				footer={() => (
+					<div className="border-top mt-3 p-3" style={{width: 450}}>
+						<div className="d-flex justify-content-end">
+							<Button
+								className="mr-3"
+								displayType="secondary"
+								onClick={() => {
+									resetForm();
 
-						<Button
-							onClick={() => {
-								onSubmit(objectInputRef.current.value);
-							}}
-							small
-						>
-							{Liferay.Language.get('continue')}
-						</Button>
+									onCancel();
+								}}
+								small
+							>
+								{Liferay.Language.get('cancel')}
+							</Button>
+
+							<Button
+								onClick={() => {
+									onSubmit(objectInputRef.current.value);
+								}}
+								small
+							>
+								{Liferay.Language.get('continue')}
+							</Button>
+						</div>
 					</div>
-				</div>
-			)}
-			ref={forwardRef}
-			showArrow={false}
-			title={() => (
-				<>
-					<h4 className="mb-3">{Liferay.Language.get('new-app')}</h4>
+				)}
+				ref={forwardRef}
+				showArrow={false}
+				title={() => (
+					<>
+						<h4 className="mb-3">
+							{Liferay.Language.get('new-app')}
+						</h4>
 
-					<span className="font-weight-light text-secondary">
-						{Liferay.Language.get(
-							'create-an-app-to-manage-the-data-of-an-object'
-						)}
-					</span>
-				</>
-			)}
-			visible={visible}
-		/>
+						<span className="font-weight-light text-secondary">
+							{Liferay.Language.get(
+								'create-an-app-to-manage-the-data-of-an-object'
+							)}
+						</span>
+					</>
+				)}
+				visible={visible}
+			/>
+
+			<CustomObjectPopover
+				alignElement={alignElement}
+				onCancel={handleOnCancel}
+				onSubmit={handleOnSubmit}
+				ref={popoverRef}
+				visible={isPopoverVisible}
+			/>
+		</>
 	);
 };
 
