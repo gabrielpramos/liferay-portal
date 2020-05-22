61b1bcc1e384e3392ffb501685e07bd49bd5071a
Inácio Nery <inacio.nery@liferay.com>
2020-05-22 14:09:30 -0300

    LPS-111266 | LPS-113932 Move to a component

--- modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/portlet/AppsPortlet.java
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/portlet/AppsPortlet.java
@@ -14,23 +14,20 @@
 
 package com.liferay.app.builder.web.internal.portlet;
 
-import com.liferay.app.builder.constants.AppBuilderAppConstants;
 import com.liferay.app.builder.constants.AppBuilderPortletKeys;
 import com.liferay.app.builder.portlet.tab.AppBuilderAppsPortletTab;
 import com.liferay.app.builder.web.internal.constants.AppBuilderWebKeys;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.portlet.Portlet;
@@ -73,21 +70,7 @@ public class AppsPortlet extends MVCPortlet {
 		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		Map<String, Object> appsTabs = LinkedHashMapBuilder.<String, Object>put(
-			"standard",
-			HashMapBuilder.<String, Object>put(
-				"label",
-				LanguageUtil.get(
-					ResourceBundleUtil.getModuleAndPortalResourceBundle(
-						themeDisplay.getLocale(), getClass()),
-					"standard")
-			).put(
-				"pluginEntryPoint",
-				_npmResolver.resolveModuleName("app-builder-web/js/index.es")
-			).put(
-				"scope", AppBuilderAppConstants.STANDARD_APP_SCOPE
-			).build()
-		).build();
+		Map<String, Object> appsTabs = new LinkedHashMap<>();
 
 		for (String appBuilderAppsPortletTabName :
 				_appBuilderAppsPortletTabTrackerMap.keySet()) {
