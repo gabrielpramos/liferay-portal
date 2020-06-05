b263318355dc04b654a54689afed03bfe2afb9a8
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-06-05 12:21:36 -0300

    WIP

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/workload-by-step-card/WorkloadByStepCard.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/workload-by-step-card/WorkloadByStepCard.es.js
@@ -9,9 +9,11 @@
  * distribution rights of the Software.
  */
 
-import React, {useMemo} from 'react';
+import ClayButton from '@clayui/button';
+import ClayDropDown, {ClayDropDownWithItems} from '@clayui/drop-down';
+import {ClayRadio, ClayRadioGroup} from '@clayui/form';
+import React, {useMemo, useState} from 'react';
 
-import Panel from '../../../shared/components/Panel.es';
 import PromisesResolver from '../../../shared/components/promises-resolver/PromisesResolver.es';
 import {useFetch} from '../../../shared/hooks/useFetch.es';
 import {Body} from './WorkloadByStepCardBody.es';
@@ -23,21 +25,102 @@ const WorkloadByStepCard = ({processId, routeParams}) => {
 	});
 
 	const promises = useMemo(() => [fetchData()], [fetchData]);
+	const [active, setActive] = useState(false);
+	const [value, setValue] = useState('one');
+
+	const createItem = (size) => {
+		const item = [];
+
+		for (let i = 0; i < size; i++) {
+			item.push({checked: false, label: `item${i}`, value: i});
+		}
+
+		return item;
+	};
+
+	const [items, setItems] = useState(createItem(5));
+
+	const onChange = (index) => {
+		setItems([
+			...items.map((item, indexItem) => ({
+				...item,
+				checked: index === indexItem,
+			})),
+		]);
+	};
+
+	const items2 = [
+		{
+			items: [
+				{
+					checked: true,
+					label: 'one',
+					type: 'radio',
+					value: 'one',
+				},
+				{
+					label: 'two',
+					type: 'radio',
+					value: 'two',
+				},
+			],
+			label: 'radio',
+			name: 'radio',
+			type: 'radiogroup',
+			value: 'one',
+		},
+	];
 
 	return (
 		<PromisesResolver promises={promises}>
-			<Panel className="container-fluid-1280 mt-4">
-				<Panel.HeaderWithOptions
-					description={Liferay.Language.get(
-						'workload-by-step-description'
-					)}
-					elementClasses="dashboard-panel-header"
-					title={Liferay.Language.get('workload-by-step')}
-					tooltipPosition="bottom"
-				/>
+			<ClayDropDown
+				active={active}
+				onActiveChange={setActive}
+				trigger={<ClayButton>Click Here</ClayButton>}
+			>
+				<ClayDropDown.ItemList>
+					<ClayDropDown.Group header={'Radio'}>
+						<ClayDropDown.ItemList>
+							{items.map(({checked, label, value}, index) => (
+								<ClayDropDown.Section key={index}>
+									<ClayRadio
+										checked={checked}
+										label={label}
+										onChange={(event) => {
+											event.stopPropagation();
+											onChange(index);
+										}}
+										value={value}
+									/>
+								</ClayDropDown.Section>
+							))}
+						</ClayDropDown.ItemList>
+					</ClayDropDown.Group>
+				</ClayDropDown.ItemList>
+			</ClayDropDown>
 
-				<WorkloadByStepCard.Body {...data} {...routeParams} />
-			</Panel>
+			<ClayRadioGroup
+				inline
+				onSelectedValueChange={(val) => setValue(val)}
+				selectedValue={value}
+			>
+				<ClayRadio label="One" value="one" />
+				<ClayRadio label="Two" value="two" />
+				<ClayRadio label="Three" value="three" />
+			</ClayRadioGroup>
+
+			<ClayDropDownWithItems
+				footerContent={
+					<>
+						<ClayButton displayType="secondary">
+							{'Cancel'}
+						</ClayButton>
+						<ClayButton>{'Done'}</ClayButton>
+					</>
+				}
+				items={items2}
+				trigger={<ClayButton>{'Click Me'}</ClayButton>}
+			/>
 		</PromisesResolver>
 	);
 };
