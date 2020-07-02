2d565d5ef8d3f2e34b3956aefd7e0bd26ab6d8de
Nara Andrade <nara.andrade@liferay.com>
2020-07-02 10:56:59 -0300

    LPS-115735 | LPS-110981 Fetch  roles and display assignee component

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/edit/EditApp.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/edit/EditApp.es.js
@@ -53,6 +53,7 @@ export default ({
 		getInitialConfig()
 	);
 
+	const [assigneeRoles, setAssigneeRoles] = useState([]);
 	const [isModalVisible, setModalVisible] = useState(false);
 	const [isLoading, setLoading] = useState(false);
 
@@ -67,6 +68,10 @@ export default ({
 	};
 
 	useEffect(() => {
+		getItem('/o/headless-admin-user/v1.0/roles').then(({items}) =>
+			setAssigneeRoles(items)
+		);
+
 		if (appId) {
 			setLoading(true);
 
@@ -139,6 +144,11 @@ export default ({
 									config.steps.some(
 										({name}) =>
 											!name || name.trim().length === 0
+									) ||
+									config.steps.some(
+										(step) =>
+											step.initial === undefined &&
+											step.roleIds.length === 0
 									)
 								}
 								onClick={() => setModalVisible(true)}
@@ -150,7 +160,7 @@ export default ({
 
 					<WorkflowBuilder />
 
-					<EditAppSidebar />
+					<EditAppSidebar assigneeRoles={assigneeRoles} />
 
 					<DeployAppModal onCancel={onCancel} />
 				</EditAppContext.Provider>
